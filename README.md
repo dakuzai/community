6f43b7b26330d6a00297c95af766f30e05e944e8
dakuzai <569054877@qq.com>
2019-09-16 11:25:15 +0800

    add index and bootstrap

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ https://github.com/dakuzai/community
 
 ## 资料
 https://spring.io/guides/gs/serving-web-content/
+https://v3.bootcss.com/css/#grid
+https://developer.github.com/
+https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/
 
 ## 工具
 https://git-scm.com/downloads
--- src/main/java/life/dakuzai/community/community/controller/IndexController.java
+++ src/main/java/life/dakuzai/community/community/controller/IndexController.java
@@ -13,16 +13,11 @@ import org.springframework.web.bind.annotation.RequestParam;
  *@Date 2019/9/12 15:42
  **/
 @Controller
-public class HelloController {
+public class IndexController {
 
-    @GetMapping("hello")
-    public String hello(@RequestParam(name = "name") String name, Model model){
-
-        model.addAttribute("name",name);
-
-        return "hello";
-
-
+    @GetMapping("/")
+public String index(){
+        return "index";
     }
 
 
