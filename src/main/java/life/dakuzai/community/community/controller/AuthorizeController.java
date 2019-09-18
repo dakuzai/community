eab59b5e71d16500ea5ac5cd2779cd1dd5b9de73
dakuzai <569054877@qq.com>
2019-09-18 09:50:45 +0800

    update index and AuthorizeController

--- src/main/java/life/dakuzai/community/community/controller/AuthorizeController.java
+++ src/main/java/life/dakuzai/community/community/controller/AuthorizeController.java
@@ -1,6 +1,4 @@
-package life.dakuzai.community.community.controller;/**
- * Created by admin on 2019/9/16 14:30
- */
+package life.dakuzai.community.community.controller;
 
 import life.dakuzai.community.community.dto.AccessTokenDTO;
 import life.dakuzai.community.community.dto.GithubUser;
@@ -10,6 +8,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  *@ClassName AuthorizeController
@@ -30,7 +30,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -39,7 +40,13 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if (user != null){
+            //登录成功，写cookie和session    重定向
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        }else{
+            //登陆失败
+            return "redirect:/";
+        }
     }
 }
